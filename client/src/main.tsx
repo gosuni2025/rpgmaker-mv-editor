b2d6346efb7595ba7baad73611e81a7db4f0be2e
gosuni2025 <gosuni2025@gmail.com>
2026-02-11 17:45:24 +0900

    Revert "개발 모드에서 js-error-overlay 적용 (에디터 + 플레이어)"
    This reverts commit 71f59019fd68284a035467b7ad81dffbca57bb86.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './i18n';
 import App from './App';
 
-if (import.meta.env.DEV) {
-  import('js-error-overlay').then(({ install }) => install());
-}
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
